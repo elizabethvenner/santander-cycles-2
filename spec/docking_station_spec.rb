20570d9e4f60226d65ff9e59659c2253f43d92c5
elizabethvenner <elizabethvenner@googlemail.com>
2016-09-29 12:58:18 +0100

    add method to create bike and method to check if bike working

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,13 +1,17 @@
 require 'docking_station'
 
-describe DockingStation do
-	
-   	it {is_expected.to respond_to :release_bike} 
-	
 
-	describe 'create new instance of bike class using release_bike' do
-    it {is_expected.to :release_bike}
-	end
+describe DockingStation do
+  station = DockingStation.new
+   	it {is_expected.to respond_to :release_bike}
+
+
+	  it 'creates a new instance of bike class using release_bike' do
+		  expect(station.release_bike).to be_an_instance_of Bike
+		end
+
+		it 'checks whether the new instance of the bike class is_working?' do
+			expect(Bike.new).to be_working	
+	  end
 
 end
-
