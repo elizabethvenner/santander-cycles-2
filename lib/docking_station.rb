ffda26338c3959aae31d88d788576a4e4bb0eab4
Stuart Cooper <stucooper86@gmail.com>
2016-09-29 15:16:05 +0100

    Adding method test for docking a bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,6 +1,14 @@
 class DockingStation
 
 	def release_bike
-		Bike.new	
+		Bike.new
 	end
+
+	#attr_reader :dock or :bike
+
+	def dock(bike)
+		true
+	end
+
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -5,13 +5,18 @@ describe DockingStation do
   station = DockingStation.new
    	it {is_expected.to respond_to :release_bike}
 
-
 	  it 'creates a new instance of bike class using release_bike' do
 		  expect(station.release_bike).to be_an_instance_of Bike
 		end
 
 		it 'checks whether the new instance of the bike class is_working?' do
-			expect(Bike.new).to be_working	
+			expect(Bike.new).to be_working
 	  end
+
+    it {is_expected.to respond_to :dock }
+
+    it 'docks an instance of bike at the given docking station' do
+      expect(station.dock(Bike.new)).to eq true
+    end
 
 end
